79fa2af226f15089ad18c1d81f5654c64f5a2837
jeyefendi <jeyhunfarajov@gmail.com>
2022-08-12 01:32:46 +0300

    completed resume coponent of aboutPage

--- components/About/Resume.jsx
+++ components/About/Resume.jsx
@@ -1,9 +1,18 @@
 import styles from "./About.module.css";
+import Image from "next/image";
 
 const Resume = () => {
   return (
     <div className={styles.resume}>
-        Look at my <span>CV</span>
+        <div className={styles.resume_text}>
+          <h2>Looking for my CV?</h2>
+          <p>Choose your language:</p>
+        </div>
+
+        <div className={styles.resume_icons}>
+          <a  target="_blank" rel="noopener noreferrer" className={styles.language} href='https://drive.google.com/file/d/1ICP8EB2NTUNTEMp0fTxaine4arFL5FyB/view?usp=sharing'><Image src="/about/english.webp" alt="English" layout="fill"/></a>
+          <a  target="_blank" rel="noopener noreferrer" className={styles.language} href='https://drive.google.com/file/d/1sBcDhj8gwnJ0Jp97RO-CNdl6YMVB_GO_/view?usp=sharing'><Image src="/about/russian.webp" alt="Russian" layout="fill"/></a>
+        </div>        
     </div>
   )
 }
